7ce02b71c4beccd715879343e3a5da33643d8a4f
Yashvi-s17734 <yashvipshah80@gmail.com>
2026-01-20 09:51:56 +0530

    username via otp

--- backend/utils/sendOtp.js
+++ backend/utils/sendOtp.js
@@ -1,15 +1,15 @@
-const sgMail=require("@sendgrid/mail");
+const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const sendOtpEmail=async(email,otp)=>{
-    await sgMail.send({
-      to: email,
-      from: process.env.FROM_EMAIL,
-      subject: "Your OTP Verification Code",
-      html: `
+const sendOtpEmail = async (email, otp) => {
+  await sgMail.send({
+    to: email,
+    from: process.env.FROM_EMAIL,
+    subject: "Your login code",
+    html: `
       <h2>Your OTP is: ${otp}</h2>
       <p>This OTP is valid for 5 minutes.</p>
       <p>Do not share this OTP.</p>
     `,
-    });
-}
-module.exports=sendOtpEmail;
+  });
+};
+module.exports = sendOtpEmail;
