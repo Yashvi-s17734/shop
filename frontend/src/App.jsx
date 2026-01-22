6f4a4acaec5a524efba83faf785a0318ce48fcbe
Yashvi-s17734 <yashvipshah80@gmail.com>
2026-01-22 21:10:42 +0530

    home page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,7 @@
-import Login from "./pages/Login";
 import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import OAuthSuccess from "./pages/OAuthSuccess";
-import ForgotPassword from "./pages/ForgotPassword";
-import OtpVerify from "./pages/OtpVerify";
-import ResetPassword from "./pages/ResetPassword";
 
-export default function App() {
-  return (
-    <>
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          duration: 3000,
-        }}
-      />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/oauth-success" element={<OAuthSuccess />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/verify-otp" element={<OtpVerify />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-      </Routes>
-    </>
-  );
+function App() {
+  return <Home />;
 }
+
+export default App;
